f3bf6f0dde6193cd56f841f7f6d317b4de96a068
Ryan Stokes <ryanstokes560@gmail.com>
2025-11-07 20:53:56 +0000

    Added further changes to solid queue

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,9 @@
+# Disable Solid Queue completely in production before Rails boots
+if ENV["RAILS_ENV"] == "production" || ENV["RACK_ENV"] == "production"
+  ENV["SOLID_QUEUE_DISABLED"] = "true"
+  puts "🧩 Solid Queue disabled (boot.rb)"
+end
+
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
